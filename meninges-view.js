58e939137d1b90a40c03afc982e788759f4d08e0
Peter Murray <pj.murray.nz@gmail.com>
2011-10-17 15:13:57 +1100

    added check in view to not set value if it hasn't change or has changed from null to empty string

--- meninges-view.js
+++ meninges-view.js
@@ -3,16 +3,16 @@ Backbone.MeningesView = {
 
     var findNextModel = function (startingModel, pathElement) {
       if (pathElement.indexOf(":") === -1) {
-          return startingModel.get(pathElement);
-        }
-        else {
-          var a = pathElement.split(":");
-          return startingModel.get(a[0]).at(parseInt(a[1]));
-        }
+        return startingModel.get(pathElement);
+      }
+      else {
+        var a = pathElement.split(":");
+        return startingModel.get(a[0]).at(parseInt(a[1]));
+      }
     };
 
     var extractValue = function (element) {
-      if(element.type === "checkbox") {
+      if (element.type === "checkbox") {
         return element.checked;
       }
       return element.value || '';
@@ -24,14 +24,21 @@ Backbone.MeningesView = {
     o.events["change input.meninges[type='radio']"] = 'updateAttributeForEvent';
     o.events["change select.meninges"] = 'updateAttributeForEvent';
 
-    o.updateAttribute = function(pathItems, value, currentModel){
+    o.updateAttribute = function(pathItems, value, currentModel) {
       for (var i = 0; i < pathItems.length - 1; i++) {
         currentModel = findNextModel(currentModel, pathItems[i]);
       }
       var newValueHash = {};
+      var oldValue = currentModel.get(_(pathItems).last());
+      var sameValue = (oldValue === value);
+      var nullToEmpty = (value === '' && oldValue === null);
+
       newValueHash[_(pathItems).last()] = value;
-      currentModel.set(newValueHash);
-    }
+
+      if (!sameValue && !nullToEmpty) {
+        currentModel.set(newValueHash);
+      }
+    };
 
     o.updateAttributeForEvent = function (event) {
       var pathItems = event.target.name.split(".");
